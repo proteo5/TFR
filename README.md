848c08f6f46e890555c0f9ed51f0d8b933605ca2
proteo5 <proteo5@hotmail.com>
2024-06-15 10:48:58 -0700

    Project Creation

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# TFR
-TFR - Temporary Files Remover
+# Temporary File Remover (TFR)
+
+Temporary File Remover (TFR) is a console application designed to help you regularly delete temporary files and folders on your system. It reads a list of file paths from a specified text-based paths file and deletes those files and the folders inside of them, optionally skipping confirmation prompts.
+
+## Features
+- Deletes files and folders based on a list of paths provided in a text file.
+- Option to skip delete confirmation prompts.
+- Displays help and version information.
+
+## Usage
+
+```sh
+TFR [--skip-confirmation] [--help] [--version] path-file
+```
+
+## Arguments
+- path-file: Specify a paths file containing the list of files to delete. If no path is provided then it will look for the file paths.txt.
+
+## Options
+- -s, --skip-confirmation: Skip the delete confirmation prompt.
+- -h, --help: Show help message.
+- --version: Show version information.
+
+## Example
+
+```
+TFR /path/to/your/paths-file.txt
+```
+
+## Paths File Format
+The paths file should contain one file path per line.  
+Also they can contain comments by starting the line with the "#" caracter.  
+Empty lines will be ignored.  
+For example:
+
+```
+# Group 1
+C:\Temp\file1.tmp
+C:\Temp\file2.tmp
+C:\Temp\file3.tmp
+
+# Group 2
+C:\Temp2\file1.tmp
+```
+## Acknowledgments
+
+* Thanks to [Mayuki](https://github.com/mayuki) for your project Cocona [https://github.com/mayuki/Cocona](https://github.com/mayuki/Cocona), doing the console app with your framework was a breeze.
+* Thanks for the icon to Flaticon [Delete folder icon created by juicy_fish - Flaticon](https://www.flaticon.com/free-icons/delete-folder)
+
+## Authors
+
+- Alfredo Pinto Molina (aka proteo5) [https://github.com/proteo5](https://github.com/proteo5)
+
+## License
+This project is licensed under the MIT License.
+
+Copyright 2024 Alfredo Pinto Molina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ TFR/Program.cs
@@ -0,0 +1,183 @@
+﻿using Cocona;
+
+namespace TFR;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = CoconaApp.CreateBuilder();
+        var app = builder.Build();
+
+        app.AddCommand((
+            [Argument(Description = "Specify a paths file")]
+            string? pathFile,
+            [Option('s', Description = "Skip delete confirmation")]
+            bool skipConfirmation
+            ) =>
+        {
+            #region Validations
+
+            //check if the string pathsFilepathArg is empty, otherwise use the default value
+            string pathsFilepath = string.IsNullOrEmpty(pathFile) ?
+                            pathsFilepath = Path.Combine(Directory.GetCurrentDirectory(), "paths.txt") :
+                            pathFile;
+
+            Console.WriteLine($"Paths file: {pathsFilepath}");
+            Console.WriteLine();
+
+            //check if the string files has less than 2000 characters
+            if (pathsFilepath.Length > 2000)
+            {
+                Console.WriteLine($"The maximum number of characters for a paths file is 2000 ({pathsFilepath.Length})");
+                return;
+            }
+
+            //check if the file exists
+            if (!File.Exists(pathsFilepath))
+            {
+                Console.WriteLine($"The file {pathsFilepath} does not exist");
+                return;
+            }
+
+            //check if the file is empty
+            if (new FileInfo(pathsFilepath).Length == 0)
+            {
+                Console.WriteLine($"The file {pathsFilepath} is empty");
+                return;
+            }
+
+            //check if each line in the file is a valid path
+            var paths = File.ReadAllLines(pathsFilepath);
+            bool foundInvalidPath = false;
+            foreach (var path in paths)
+            {
+                //if the path is empty or if starts with #, skip it
+                if (string.IsNullOrEmpty(path) || path.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                if (!Directory.Exists(path))
+                {
+                    foundInvalidPath = true;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Path invalid: {path}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Path valid: {path}");
+                    Console.ResetColor();
+                }
+            }
+
+            if (foundInvalidPath)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please fix the invalid paths and try again");
+                return;
+            }
+
+            #endregion
+
+            //confirm if the user wants to delete the files 
+            if (!skipConfirmation)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Do you want to delete the files and folders on the paths ? (Y/N)");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+                if (key.Key != ConsoleKey.Y)
+                {
+                    Console.WriteLine("Operation canceled by user");
+                    return;
+                }
+            }
+
+            //delete the files and folders on the paths
+            foreach (var path in paths)
+            {
+                //if the path is empty or if starts with #, skip it
+                if (string.IsNullOrEmpty(path) || path.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Deleting: {path}");
+                //Directory.Delete(path, true);
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    if (!IsFileLocked(file))
+                    {
+                        file.Delete();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The file {file.FullName} is locked and can't be deleted.");
+                    }
+                }
+
+                foreach (DirectoryInfo subDirectory in directoryInfo.GetDirectories())
+                {
+                    if (!IsDirectoryLocked(subDirectory))
+                    {
+                        subDirectory.Delete(true);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The folder {subDirectory.FullName} is locked and can't be deleted.");
+                    }
+                }
+
+            }
+
+            //inidicate that the operation was successful and the number of paths deleted
+            Console.WriteLine();
+            Console.WriteLine("Operation completed successfully");
+            Console.WriteLine($"Number of paths deleted: {paths.Length}");
+            Console.WriteLine();
+
+        });
+
+        app.Run();
+    }
+
+    static bool IsFileLocked(FileInfo file)
+    {
+        try
+        {
+            using (FileStream stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+            {
+                stream.Close();
+            }
+        }
+        catch (IOException)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static bool IsDirectoryLocked(DirectoryInfo directory)
+    {
+        try
+        {
+            // Intenta abrir un archivo en el directorio para verificar si el directorio está en uso
+            string tempFilePath = Path.Combine(directory.FullName, Path.GetRandomFileName());
+            using (FileStream stream = new FileStream(tempFilePath, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None))
+            {
+                stream.Close();
+            }
+            File.Delete(tempFilePath);
+        }
+        catch (IOException)
+        {
+            return true;
+        }
+        return false;
+    }
+}
